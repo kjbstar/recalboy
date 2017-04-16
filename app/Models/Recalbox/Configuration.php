f3d0f8ef2f01a20ba8f5ca28dc2c3f722915eed4
kjbstar <kjbstar@gmail.com>
2017-04-16 12:00:10 +0200

    Enhancement : Classe configuration peut prendre n'importe quel fichier de conf, celui de recalbox par défaut.

--- app/Models/Recalbox/Configuration.php
+++ app/Models/Recalbox/Configuration.php
@@ -9,15 +9,23 @@ class Configuration extends Model
 
     private static $output;
 
-    public static function setValue($param, $value) {
+    public static function setValue($param, $value, $config = null) {
+
+        if ($config === null) {
+            $config = '/recalbox/share/system/recalbox.conf';
+        }        
 
         \SSH::run('sed -i "s/\('.$param.' *= *\).*/\1'.$value.'/" /recalbox/share/system/recalbox.conf');
             
     }
 
-    public static function getValue($param) {
+    public static function getValue($param, $config = null) {
 
-        \SSH::run('cat /recalbox/share/system/recalbox.conf | grep "^'.$param.'" | sed "s/^\(.*\)\('.$param.'=\)\(.*\)$/\3/"', function($line) {
+        if ($config === null) {
+            $config = '/recalbox/share/system/recalbox.conf';
+        }
+
+        \SSH::run('cat '.$config.' | grep "^'.$param.'" | sed "s/^\(.*\)\('.$param.'=\)\(.*\)$/\3/"', function($line) {
             self::$output = $line;
         });
 
