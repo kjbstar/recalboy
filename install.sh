3ff9a8f7e35e50dd95594f0b1d6fbe33a919bce2
kjbstar <kjbstar@gmail.com>
2017-04-17 10:07:50 +0200

    Installer: install de Composer modifiée selon doc officielle pour scripts Bash.

--- install.sh
+++ install.sh
@@ -153,10 +153,16 @@ INSTALL_COMPOSER="${INSTALL_COMPOSER:-$ANSWER_DEFAULT}"
 
 if [[ $INSTALL_COMPOSER =~ ^([yY][eE][sS]|[yY])+$ ]] ; then
 	
-	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-	php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-	php composer-setup.php
-	php -r "unlink('composer-setup.php');"
+	if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+	then
+	    >&2 echo 'ERROR: Invalid installer signature'
+	    rm composer-setup.php
+	    exit 1
+	fi
+
+	php composer-setup.php --quiet
+	RESULT=$?
+	rm composer-setup.php	
 	sudo mv composer.phar /usr/local/bin/composer
 
 	echo ""
